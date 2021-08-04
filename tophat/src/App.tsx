0e58568c3c1f9cffc697f2ee5946e5a7fd4e4172
varurajrana <varunrajrana98@gmail.com>
2021-08-03 18:31:52 -0600

    front&back connected

---
+++ tophat/src/App.tsx
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from 'react';
+import './App.css';
+import Users from './components/Users'
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+})
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Users />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
